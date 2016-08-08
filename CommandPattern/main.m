4fa37cf7916b9d50b10845c7e63de174512455e0
rhc <bawfnhaps@163.com>
2016-08-08 15:46:24 +0800

    commandPattern

--- CommandPattern/main.m
+++ CommandPattern/main.m
@@ -7,11 +7,24 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "Invoker.h"
+#import "AddRequirementCommand.h"
+#import "DeletePageCommand.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        // 首先声明调用者Invoker
+        Invoker *rhc = [[Invoker alloc]init];
+        // 定义接收者(RequiremnetGroup,PageGroup,CodeGroup）,定义一个发送给接收者的命令
+        Command *add = [[AddRequirementCommand alloc]init];
+        // 把命令交给调用者去执行
+        rhc.command = add;
+        [rhc action];
+       /*
+        Command *deleted = [[DeletePageCommand alloc]init];
+        rhc.command = deleted;
+        [rhc action];
+        */
     }
     return 0;
 }
